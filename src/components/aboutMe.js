ae04fdc462929166c749c9ca2daded9691a02a37
JakubPyt <pyt.jakub@gmail.com>
2021-04-07 11:56:17 +0200

    about-me/Create card and style for it

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -9,7 +9,6 @@ class AboutMe extends React.Component{
     }
     render(){
         return(
-            <img src={memImage}/>
             <div className="aboutMeDiv" >
                     <Container>
                         <Row>
@@ -17,6 +16,20 @@ class AboutMe extends React.Component{
                                 <img className="memImage" src={memImage} />
                             </Col>
                             <Col sm={8}>
+                                <Card className="aboutMeCard" bg="dark" text="light">
+                                    <Card.Header className="cardHeader">O mnie</Card.Header>
+                                    <Card.Title className="cardTitle">Hello World!</Card.Title>
+                                    <Card.Body>
+                                        <ul>
+                                            <li>Jestem Jakub, maniak programowania.</li>
+                                            <li>Mocno wziąłem sobie do serca tego <a href="https://i.redd.it/l5pwx2lmfsj61.jpg">mema</a>.</li>
+                                            <li>Jestem studentem w Warszawskiej Wyższej Szkole Informatyki, na kierunku Data Science.</li>
+                                            <li>Jestem absolwentem technikum informatycznego ze zdobytymi kwalifikacjami E12(budowa komputerów), E13(budowa sieci), E14(budowa i administrowanie stronami internetowymi i bazami danych).</li>
+                                            <li>Jestem samoukiem w pozostałych polach informatyki.</li>
+                                            <li>Jestem też harcerzem, dzięki czemu umiejętności miękkie u mnie nie leżą ;)</li>
+                                        </ul>
+                                    </Card.Body>
+                                </Card>
                             </Col>
                         </Row>
                     </Container>
