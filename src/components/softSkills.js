118acc01d88c600f39229edf1240759c60ea04d2
JakubPyt <pyt.jakub@gmail.com>
2021-04-07 14:14:15 +0200

    soft-skills/Add Card to softSkills.js

--- src/components/softSkills.js
+++ src/components/softSkills.js
@@ -10,6 +10,19 @@ class SoftSkills extends React.Component{
             <div className="softSkillsDiv">
                 <Row>
                     <Col sm={8}>
+                        <Card className="softSkillsCard" bg="dark" text="light">
+                        <Card.Header className="cardHeader">Umiejętności miękkie</Card.Header>
+                        <Card.Body className="cardBody">
+                            <p>Jak to programista, mam skłonności do introwertyzmu, ale potrafię odnaleźć się w każdej sytuacji. Niech potwierdzeniem moich umiejętności miękkich będą poniższe rezultaty:</p>
+                            <ul>
+                                <li>Kierowanie organizacją dwutygodniowego obozu dla 150 osób</li>
+                                <li>Kierowanie organizacją wydarzenia jednodniowego dla 180 osób</li>
+                                <li>Praca w McDonald’s</li>
+                                <li>Dowodzenie około 20 osobową grupą młodzieżową przez okres 3 lat(2015-2018)</li>
+                                <li>Dowodzenie 16 osobową grupą młodzieżową od 2018 roku, razem z którą przeprowadzaliśmy kilka znaczących projektów, takich jak remont domu dla starszej, ubogiej pani, akcje dostarczania paczek kombatantom w naszych okolicach. Tą grupą wciąż dowodzę, a o naszych przedsięwzięciach można doczytać na naszym instagramie:</li>
+                            </ul>
+                        </Card.Body>
+                        </Card>
                     </Col>
                     <Col sm={3} >
                         <img className="meImage" src={meImage} />
