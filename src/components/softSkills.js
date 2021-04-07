41ed72a94bf0b2f4b8d016f353c58220cf380855
JakubPyt <pyt.jakub@gmail.com>
2021-04-07 12:14:21 +0200

    Create softSkills.js

---
+++ src/components/softSkills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SoftSkills extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <h1>Hello World from soft Skills</h1>
+        )
+    }
+}
+
+export {SoftSkills}
