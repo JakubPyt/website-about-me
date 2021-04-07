b9297f3c687376cdda34400c475a73be419a96d1
JakubPyt <pyt.jakub@gmail.com>
2021-04-07 10:02:58 +0200

    Create aboutMe.js
    Create aboutMe.js and add Hello World to it

---
+++ src/components/aboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutMe extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <h1>Hello World from aboutMe</h1>
+        )
+    }
+}
+
+export {AboutMe}
