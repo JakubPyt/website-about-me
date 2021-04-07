0860387ef001a060af916aa951cfaa71e4a031df
JakubPyt <pyt.jakub@gmail.com>
2021-04-07 14:10:20 +0200

    soft-skills/Add grid
    Add grid for content in softSkills.js

--- src/components/softSkills.js
+++ src/components/softSkills.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import {Card, Col, Row} from 'react-bootstrap';
 class SoftSkills extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
         return(
-            <h1>Hello World from soft Skills</h1>
+            <div className="softSkillsDiv">
+                <Row>
+                    <Col sm={8}>
+                    </Col>
+                    <Col sm={3} >
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
