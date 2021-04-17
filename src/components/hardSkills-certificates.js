1dc701f49b2b011997efa260f5b6992634f5584d
JakubPyt <pyt.jakub@gmail.com>
2021-04-17 17:13:51 +0200

    hard-skills/Create hardSkills-certificates.js

---
+++ src/components/hardSkills-certificates.js
@@ -0,0 +1,14 @@
+import React from "react";
+class HardSkillsCertificates extends React.Component{
+    constructor(props){
+        super(props);
+        }
+
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+export { HardSkillsCertificates };
