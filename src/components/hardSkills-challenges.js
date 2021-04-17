b0abf8147acd849bd29c3c012bef264ea7c09357
JakubPyt <pyt.jakub@gmail.com>
2021-04-17 17:35:08 +0200

    hard-skills/Create hardSkills-challenges.js

---
+++ src/components/hardSkills-challenges.js
@@ -0,0 +1,14 @@
+import React from "react";
+class HardSkillsChallenges extends React.Component {
+    constructor(props){
+        super(props);
+        }
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+export {HardSkillsChallenges};
