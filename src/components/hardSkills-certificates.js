fdcf2b3c35d3120cc914229aeb7e4bb0f80c9557
JakubPyt <pyt.jakub@gmail.com>
2021-04-17 17:17:28 +0200

    hard-skills/certificates/Import pics of certificates

--- src/components/hardSkills-certificates.js
+++ src/components/hardSkills-certificates.js
@@ -1,4 +1,19 @@
 import React from "react";
+import SL_SQL from '../images/certificates/SL_SQL.jpg';
+import Kaggle_Python from "../images/certificates/Kaggle_Python.jpg";
+import SL_CSharp from "../images/certificates/SL_CSharp.jpg";
+import SL_CSS from "../images/certificates/SL_CSS.jpg";
+import SL_DataScience from "../images/certificates/SL_DataScience.jpg";
+import SL_HTML from "../images/certificates/SL_HTML.jpg";
+import SL_IntermediatePython from "../images/certificates/SL_IntermediatePython.jpg";
+import SL_JS from "../images/certificates/SL_JS.jpg";
+import SL_MachineLearning from "../images/certificates/SL_MachineLearning.jpg";
+import SL_Python3 from "../images/certificates/SL_Python3.jpg";
+import SL_PythonDataStructures from "../images/certificates/SL_PythonDataStructures.jpg";
+import SL_PythonForBeginners from "../images/certificates/SL_PythonForBeginners.jpg";
+import Udemy_CSharp from "../images/certificates/Udemy_CSharp.jpg";
+import Udemy_FullStack from "../images/certificates/Udemy_FullStack.jpg";
+import Udemy_Git from "../images/certificates/Udemy_Git.jpg";
 class HardSkillsCertificates extends React.Component{
     constructor(props){
         super(props);
