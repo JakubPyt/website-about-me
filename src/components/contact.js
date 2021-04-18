86716d51aaa329c49af786fe45c919c14b920793
JakubPyt <pyt.jakub@gmail.com>
2021-04-18 13:01:45 +0200

    contact/Add links to icons and stylize it

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,24 +14,36 @@ class Contact extends React.Component {
             <div className="contactDiv">
                 <h1>Kontakt</h1>
                 <p>Zapraszam do kontaktu na profilu LinkedIn</p>
+                        <a href="https://www.linkedin.com/in/jakub-pyt-9449b4204/">
                             <div className="circleForIcons">
                                 <AiFillLinkedin className="icon"/>
                             </div>
+                        </a>
+                        <a href="https://github.com/JakubPyt">
                             <div className="circleForIcons">
                                 <AiFillGithub className="icon"/>
                             </div>
+                        </a>
+                        <a href="https://www.sololearn.com/profile/16942149">
                             <div className="circleForIcons">
                                 <AiOutlineSlack className="icon"/>
                             </div>
+                        </a>
+                        <a href="https://www.freecodecamp.org/jakubpyt">
                             <div className="circleForIcons">
                                 <FaFreeCodeCamp className="icon"/>
                             </div>
+                        </a>
+                        <a href="https://www.hackerrank.com/pyt_jakub?hr_r=1">
                             <div className="circleForIcons">
                                 <SiHackerrank className="icon"/>
                             </div>
+                        </a>
+                        <a href="https://www.codewars.com/users/pyt_jakub">
                             <div className="circleForIcons">
                                 <SiCodewars className="icon"/>
                             </div>
+                        </a>
             </div>
         );
     }
