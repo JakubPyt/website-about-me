e6c94b6a8fd6c767ee2b90b1fcc878ca2bc9e66e
JakubPyt <pyt.jakub@gmail.com>
2021-04-18 16:13:55 +0200

    Create navigation.js
    Create navigation.js and add this to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navigation} from './components/navigation';
 import {Header} from './components/header'; 
 import {AboutMe} from './components/aboutMe';
 import {SoftSkills} from './components/softSkills';
 import {HardSkills} from './components/hardSkills';
 import {Contact} from './components/contact';
 
+
 function App() {
   return (
     <div className="">
+      <Navigation/>
       <Header/>
       <AboutMe/>
       <SoftSkills/>
---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export { Navigation };
