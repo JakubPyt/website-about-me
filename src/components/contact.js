e411a0f1289f1cff2f9e2776d0ebfffefff92f15
JakubPyt <pyt.jakub@gmail.com>
2021-04-18 13:03:59 +0200

    contact/Add singleProfile divs and style
    Add singleProfile divs with name of website and stylize it

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,36 +14,54 @@ class Contact extends React.Component {
             <div className="contactDiv">
                 <h1>Kontakt</h1>
                 <p>Zapraszam do kontaktu na profilu LinkedIn</p>
+                    <div className="singleProfile">
                         <a href="https://www.linkedin.com/in/jakub-pyt-9449b4204/">
                             <div className="circleForIcons">
                                 <AiFillLinkedin className="icon"/>
                             </div>
                         </a>
+                        LinkedIn
+                    </div>
+                    <div className="singleProfile">
                         <a href="https://github.com/JakubPyt">
                             <div className="circleForIcons">
                                 <AiFillGithub className="icon"/>
                             </div>
                         </a>
+                        GitHub
+                    </div>
+                    <div className="singleProfile">
                         <a href="https://www.sololearn.com/profile/16942149">
                             <div className="circleForIcons">
                                 <AiOutlineSlack className="icon"/>
                             </div>
                         </a>
+                        SoloLearn
+                    </div>
+                    <div className="singleProfile">
                         <a href="https://www.freecodecamp.org/jakubpyt">
                             <div className="circleForIcons">
                                 <FaFreeCodeCamp className="icon"/>
                             </div>
                         </a>
+                        freeCodeCamp
+                    </div>
+                    <div className="singleProfile">
                         <a href="https://www.hackerrank.com/pyt_jakub?hr_r=1">
                             <div className="circleForIcons">
                                 <SiHackerrank className="icon"/>
                             </div>
                         </a>
+                        HackerRank
+                    </div>
+                    <div className="singleProfile">
                         <a href="https://www.codewars.com/users/pyt_jakub">
                             <div className="circleForIcons">
                                 <SiCodewars className="icon"/>
                             </div>
                         </a>
+                        Codewars
+                    </div>
             </div>
         );
     }
