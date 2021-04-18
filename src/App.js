d353097526b157fcfb07b0e651748852cb1fcf74
JakubPyt <pyt.jakub@gmail.com>
2021-04-18 13:18:38 +0200

    App.js/Merge all components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Header} from './components/header'; 
+import {AboutMe} from './components/aboutMe';
+import {SoftSkills} from './components/softSkills';
 import {HardSkills} from './components/hardSkills';
+import {Contact} from './components/contact';
 
 function App() {
   return (
     <div className="">
-      <h1>Hello World!</h1>
+      <Header/>
+      <AboutMe/>
+      <SoftSkills/>
       <HardSkills/>
-
+      <Contact/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
