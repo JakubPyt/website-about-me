06d42110df60adf1b65dc493153f400fc373707c
JakubPyt <pyt.jakub@gmail.com>
2021-04-20 21:48:36 +0200

    Cleaning App.js
    Cleaning App.js from defualt view

--- src/App.js
+++ src/App.js
@@ -7,31 +7,8 @@ import {SoftSkills} from './components/softSkills';
 import {HardSkills} from './components/hardSkills';
 import {Contact} from './components/contact';
 
-
 function App() {
   return (
-    <div className="">
-      <Navigation/>
-      <Header/>
-      <AboutMe/>
-      <SoftSkills/>
-      <HardSkills/>
-      <Contact/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
   );
 }
 
