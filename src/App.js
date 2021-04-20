a940fdc3c42fc51d46405338791695dca9809e0f
JakubPyt <pyt.jakub@gmail.com>
2021-04-20 22:10:29 +0200

    Display all components in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//Import components
 import {Navigation} from './components/navigation';
 import {Header} from './components/header'; 
 import {AboutMe} from './components/aboutMe';
@@ -9,6 +11,15 @@ import {Contact} from './components/contact';
 
 function App() {
   return (
+      <div className="App">
+        {/* Display all components */}
+        <Navigation/>
+        <Header/>
+        <AboutMe/>
+        <SoftSkills/>
+        <HardSkills/>
+        <Contact/>
+      </div>
   );
 }
 
