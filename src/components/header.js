5404a444355bc8b83d642227eb5efb70b84729c6
JakubPyt <pyt.jakub@gmail.com>
2021-04-20 22:15:02 +0200

    Cleaning in header component
    Cleaning code, change name of variables, add comments

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
+//Import header image
 import mainImage from "../images/main.jpg";
-class Header extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        const imageStyle = {
-            width: '100%'
-        } 
-        return(
-            <div id="homeID">
-                <img src={mainImage} style={imageStyle}/>
-            </div>
-        )
-    }
-}
 
-export {Header}
+//Create Header component
+const Header = function () {
+  //Below are all styles for component, so there is no .css file for component
+  const imageStyle = {
+    width: "100%",
+  };
+  return (
+    <div 
+      name="home" //Anchor point for navigation 
+      id="header" //Navbar gets height of this div by id
+    >
+      <img
+        src={mainImage}
+        style={imageStyle}
+        alt="Coś z obrazkiem poszło nie tak. W każdym razie tu jestem ja na tle gór ;)"
+      />
+    </div>
+  );
+};
+
+export { Header };
