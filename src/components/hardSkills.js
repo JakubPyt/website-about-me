ba968ab9272aafcdf90afa99c6671e553c2c1d95
JakubPyt <pyt.jakub@gmail.com>
2021-04-20 22:15:23 +0200

    Cleaning in hardSkills component
    Cleaning code, change name of variables, add comments

--- src/components/hardSkills.js
+++ src/components/hardSkills.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./hardSkills.css";
-//Import icons
-import { Button } from "react-bootstrap";
+//Import icons for buttons
 import { FaPython } from "react-icons/fa";
 import { SiCsharp } from "react-icons/si";
 import { FiDatabase } from "react-icons/fi";
@@ -15,7 +14,7 @@ class HardSkills extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            //Is display containers with certificates
+            //Vars to define openness the containers with achivements
             openPython: false,
             openCsharp: false,
             openDB: false,
@@ -24,7 +23,7 @@ class HardSkills extends React.Component {
             openDataScience: false,
         };
     }
-    //Called func change state right state element for true or false  
+    //Called func change state element for true or false
     toggleContainerPython() {
         this.setState({ openPython: !this.state.openPython });
     }
@@ -43,7 +42,9 @@ class HardSkills extends React.Component {
     toggleContainerDataScience() {
         this.setState({ openDataScience: !this.state.openDataScience });
     }
-    //Called func display achivements by type
+
+    //Func return div with components certificates and challenges
+    //TypeContainer is string about type of achivements eg. "Python" and others
     renderContainer(typeContainer) {
         return (
             <div className="placeForAchivements">
@@ -55,22 +56,32 @@ class HardSkills extends React.Component {
 
     render() {
         return (
-            <div className="hardSkillsDiv" id="hardSkillsID">
-                <h1>Umiejętności twarde</h1>
-                {/* For each type there is button, which call func toggleContainer for turn on place for 
-                certificates and then call func for display cards with certificates and solved challenges */}
-                <Button onClick={this.toggleContainerPython.bind(this)} variant="success"> Python <FaPython /> </Button>
+            <div 
+                className="hardSkillsDiv" 
+                name="hardSkills" //Anchor point for navigation 
+            >
+                <h1 className="hardSkillsHeader">Umiejętności twarde</h1>
+                <p className="hardSkillsExplanation">Kliknij w zakładkę, aby zobaczyć certyfikaty i ukończone wyzwania programistyczne</p>
+
+                {/* For each type there is button, which call func toggleContainer for change value on true or false */}
+                <button onClick={this.toggleContainerPython.bind(this)} className="hardSkilsButton" > Python <FaPython /> </button>
+                {/* If var in state for type is true, then call func render container for type */}
                 {this.state.openPython === true ? this.renderContainer("Python") : null} 
-                <Button onClick={this.toggleContainerCsharp.bind(this)} variant="success"> C# <SiCsharp /> </Button>
-                {this.state.openCsharp === true ? this.renderContainer("Csharp") : null}
-                <Button onClick={this.toggleContainerDB.bind(this)} variant="success"> Bazy danych <FiDatabase /> </Button>
+                
+                <button onClick={this.toggleContainerDB.bind(this)} className="hardSkilsButton" > Bazy danych <FiDatabase /> </button>
                 {this.state.openDB === true ? this.renderContainer("Databases")  : null}
-                <Button onClick={this.toggleContainerGit.bind(this)} variant="success"> Git <DiGitBranch /> </Button>
-                {this.state.openGit === true ? this.renderContainer("Git")  : null}
-                <Button onClick={this.toggleContainerWebDev.bind(this)}variant="success"> Web Development <GiSpiderWeb /> </Button>
-                {this.state.openWebDev === true ? this.renderContainer("WebDev")  : null}
-                <Button onClick={this.toggleContainerDataScience.bind(this)} variant="success"> Data Science <BsClipboardData /> </Button>
+                
+                <button onClick={this.toggleContainerDataScience.bind(this)} className="hardSkilsButton" > Data Science <BsClipboardData /> </button>
                 {this.state.openDataScience === true ? this.renderContainer("DataScience")  : null}
+                
+                <button onClick={this.toggleContainerWebDev.bind(this)} className="hardSkilsButton" > Web Development <GiSpiderWeb /> </button>
+                {this.state.openWebDev === true ? this.renderContainer("WebDev")  : null}
+                
+                <button onClick={this.toggleContainerCsharp.bind(this)} className="hardSkilsButton" > C# <SiCsharp /> </button>
+                {this.state.openCsharp === true ? this.renderContainer("Csharp") : null}
+
+                <button onClick={this.toggleContainerGit.bind(this)} className="hardSkilsButton" > Git <DiGitBranch /> </button>
+                {this.state.openGit === true ? this.renderContainer("Git")  : null}
             </div>
         );
     }
