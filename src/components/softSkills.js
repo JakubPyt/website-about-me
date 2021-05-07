5ce98d229b97bb1c96cc7bd2945605409a9bd496
JakubPyt <pyt.jakub@gmail.com>
2021-05-07 15:00:34 +0200

    fixes/softSkills/Fixes in words

--- src/components/softSkills.js
+++ src/components/softSkills.js
@@ -38,7 +38,7 @@ const SoftSkills = function(){
                     <li>
                       Dowodzenie 16 osobową grupą młodzieżową od 2018 roku, razem
                       z którą przeprowadzaliśmy kilka znaczących projektów, takich
-                      jak remont domu dla starszej, ubogiej pani, akcje
+                      jak remont domu dla starszej, ubogiej pani, czy akcje
                       dostarczania paczek kombatantom w naszych okolicach. Tą
                       grupą wciąż dowodzę, a o naszych przedsięwzięciach można
                       doczytać na naszym instagramie:
